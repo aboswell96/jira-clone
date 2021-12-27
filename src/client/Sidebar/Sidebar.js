f44e2d4d94d41028ebb9498dd7ef27e69f13915c
Alex Boswell <aboswell778@gmail.com>
2021-12-26 18:45:10 -0800

    add search and add issue buttons

--- src/client/Sidebar/Sidebar.js
+++ src/client/Sidebar/Sidebar.js
@@ -3,6 +3,63 @@ import styled from 'styled-components';
 
 import {Link } from 'react-router-dom';
 
+import SearchIcon from '@mui/icons-material/Search';
+import AddIcon from '@mui/icons-material/Add';
+
+const Sidebar = () => {
+
+    const [sidebarWidth, SetSideBarWidth] = useState("64px");
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const mouseEnter = () => {
+        SetSideBarWidth("200px");
+        setIsExpanded(true);
+    };
+
+    const mouseLeave = () => {
+        SetSideBarWidth("64px");
+        setIsExpanded(false);
+    };
+
+    return(
+        <SidebarComponent
+            style={{width:sidebarWidth}}
+            onMouseEnter={mouseEnter}
+            onMouseLeave={mouseLeave}
+        >
+            <Link to="board" style={{ textDecoration: 'none', color:'inherit' }}>
+                <Icon
+                    src="https://i.ibb.co/fGyrz6b/jira-icon.png" 
+                    alt="Icon"
+                    expanded={isExpanded}
+                />
+            </Link>
+            <NavOptions>
+                <Tab expanded={isExpanded}>
+                    <ProjectIcon
+                        expanded={isExpanded}
+                    >
+                    <SearchIcon sx={{ color: "white",'fontSize':30}}/>
+                    </ProjectIcon>
+                    <Title>
+                        {isExpanded && "Search Issues"}
+                    </Title>
+                </Tab>
+                <Tab expanded={isExpanded}>
+                    <ProjectIcon
+                        expanded={isExpanded}
+                    >
+                    <AddIcon sx={{ color: "white",'fontSize':30}}/>
+                    </ProjectIcon>
+                    <Title>
+                        {isExpanded && "Create Issue"}
+                    </Title>
+                </Tab>
+            </NavOptions>
+        </SidebarComponent>
+    );
+}
+
 const Icon = styled.img`
     height:32px;
     width:32px;
@@ -10,7 +67,7 @@ const Icon = styled.img`
     display:block;
     padding-top: 35px;
 
-    ${({expanded}) => `
+    ${({expanded}) => expanded && `
         margin-left:15px;
     `}
 `
@@ -23,33 +80,41 @@ const SidebarComponent = styled.div`
     position: absolute;
 `
 
-const Sidebar = () => {
+const NavOptions = styled.div`
+    padding-top:30px;
+`
 
-    const [sidebarWidth, SetSideBarWidth] = useState("64px");
+const Tab = styled.div`
+    width: 100%;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 3px;
+    overflow: hidden;
+    font-family: CircularStdBook;
+    transition: background 0.1s ease 0s;
 
-    const mouseEnter = () => {
-        SetSideBarWidth("200px");
-    };
+    ${({ expanded }) => expanded && `
+        gap: 15px;
+    `}
 
-    const mouseLeave = () => {
-        SetSideBarWidth("64px");
-    };
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+    }
+`
 
-    return(
-        <SidebarComponent
-            style={{width:sidebarWidth}}
-            onMouseEnter={mouseEnter}
-            onMouseLeave={mouseLeave}
-            >
-            <Link to="board" style={{ textDecoration: 'none', color:'inherit' }}>
-                <Icon
-                    src="https://i.ibb.co/fGyrz6b/jira-icon.png" 
-                    alt="Icon"
-                    expanded={sidebarWidth}
-                />
-            </Link>
-            </SidebarComponent>
-    );
-}
+const Title = styled.div`
+    color: white;
+    font-size: 14.7px;
+`
+
+const ProjectIcon = styled.div`
+    margin: auto;
+
+    ${({expanded}) => expanded && `
+        margin:0;
+    `}
+`
 
 export default Sidebar;
