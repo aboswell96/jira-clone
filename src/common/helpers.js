b5b8e83a02d430eaa6aca4655cdf0e1510e6dd46
Alex Boswell <aboswell778@gmail.com>
2022-03-29 19:05:34 -0700

    add helper function

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,3 +1,9 @@
 export function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
+
+//Input: Array of Objects, attribute to pluck
+//Output: Array of values under that attribute
+export function pluck(array, attribute) {
+  return array.map((obj) => obj[attribute]);
+}
